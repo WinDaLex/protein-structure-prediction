a6b7daa6b99ed0b5db26b5de34edd7f90aa2ee2a
WinDaLex <windalex6@gmail.com>
2015-02-23 13:08:33 +0000

    Remove MapReduce dir

--- R/train.R
+++ R/train.R
@@ -1,13 +1,25 @@
 library(neuralnet)
 
-train <- function(filename, window.size) {
+train <- function(filename, weights=NULL) {
     ptm <- proc.time()
-    w <- window.size * 20
-    formula <- paste(paste("V", paste(seq(w+1,w+4), collapse="+V"), sep=""), paste(" ~ V", paste(seq(1,w), collapse="+V"), sep=""), sep="")
+
     data <- read.csv(filename)
+    m <- dim(data)[1]
+    n <- dim(data)[2]
 
+    w <- n - 5
 
-    model <- neuralnet(formula, data, hidden=5)
+    formula <- paste(paste("V", paste(seq(w+1,w+4), collapse="+V"), sep=""), paste(" ~ V", paste(seq(1,w), collapse="+V"), sep=""), sep="")
+
+    model <- neuralnet(formula, data[1:m%/%10,], hidden=5, startweights=weights)
+    print("1/10")
+    for (i in 1:8) {
+        model <- neuralnet(formula, data[(m%/%10*i)+1:as.integer((m%/%10*(i+1))),], hidden=5, startweights=model$weights)
+        cat(i+1)
+        print("/10")
+    }
+    model <- neuralnet(formula, data[as.integer((m%/%10*9)+1):m,], hidden=5, startweights=model$weights)
+    print("10/10")
 
     print(proc.time() - ptm)
 
