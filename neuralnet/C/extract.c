7124bc7e6fd2ad0b9ea3baf5ec1db7c76174ae8d
WinDaLex <windalex6@gmail.com>
2015-02-27 23:38:48 +0000

    Update C implement of neural network prediction

--- neuralnet/C/extract.c
+++ neuralnet/C/extract.c
@@ -1,56 +1,55 @@
+/*
+ * extract.c: Extract data from source file to target file according to
+ * window_size and num_examples.
+ */
+
 #include <stdio.h> 
 #include <string.h>
 
 const char BASIC_AMINO_ACIDS[20] = {
         'A', 'R', 'N', 'D', 'C', 'E', 'Q', 'G', 'H', 'I', 'L', 'K', 'M', 'F',
-        'P', 'S', 'T', 'W', 'Y', 'V'};
+        'P', 'S', 'T', 'W', 'Y', 'V'
+};
 const char HETC[4] = {'H', 'E', 'T', 'C'};
 
 const int MAX_INPUT_LENGTH = 100002;
 
 int main(int argc, char *argv[]) {
-    if (argc < 5) {
-        fprintf(stderr, "usage: %s source target window_size num_examples\n", argv[0]);
-        return 1;
+    if (argc < 4) {
+        fprintf(stderr, "usage: %s source target window_size [num_examples]\n", argv[0]);
+        return -1;
     }
 
     const char *source = argv[1];
     const char *target = argv[2];
 
-    unsigned int window_size;
-    if (sscanf(argv[3], "%u", &window_size) != 1) {
-        fprintf(stderr, "error: argument window_size should be a unsigned integer\n");
-        return 2;
-    } else if (window_size <= 0 || window_size % 2 == 0) {
-        fprintf(stderr, "error: argument window_size should be odd positive number\n");
-        return 2;
-    }
+    int window_size;
+    sscanf(argv[3], "%d", &window_size);
 
     int num_examples = -1;
-    if ((sscanf(argv[4], "%d", &num_examples) != 1) || num_examples <= 0) {
-        fprintf(stderr, "num_examples should not be positive integer\n");
-        return 3;
-    }
-
+    if (argc >= 4) sscanf(argv[4], "%d", &num_examples);
 
     FILE *source_file = fopen(source, "r");
     if (!source_file) {
         fprintf(stderr, "Cannot access the file '%s'\n", source);
-        return 3;
+        return -2;
     }
 
-    char primary[MAX_INPUT_LENGTH], hetc[MAX_INPUT_LENGTH];
+    puts("Extracting data...");
 
     FILE *target_file = fopen(target, "w");
-    fprintf(target_file, "%d %d %d\n", num_examples, window_size * 20, 4);
+    fprintf(target_file, "? %d %d\n", window_size * 20, 4);
 
+    char primary[MAX_INPUT_LENGTH], hetc[MAX_INPUT_LENGTH];
     fscanf(source_file, "%*s %*s %*s");
     while (fscanf(source_file, "%*s %*s %s %s", primary, hetc) != EOF && num_examples--) {
         size_t len = strlen(primary);
         for (int i = 1; i < len - 1; i++) {
-            for (int j = i - (int)window_size / 2; j <= i + (int)window_size / 2; j++) {
+
+            for (int j = i - window_size / 2; j <= i + window_size / 2; j++) {
                 for (int k = 0; k < 20; k++) {
-                    fputc(primary[j < 1 || j >= len - 1 ? 0 : j] == BASIC_AMINO_ACIDS[k] ? '1' : '0', target_file);
+                    int pos = j < 1 || j >= len - 1 ? 0 : j;
+                    fputc(primary[pos] == BASIC_AMINO_ACIDS[k] ? '1' : '0', target_file);
                     fputc(' ', target_file);
                 }
             }
--- neuralnet/C/test.c
+++ neuralnet/C/test.c
@@ -1,42 +1,63 @@
+/*
+ * test.c: Testing program
+ */
+
 #include <stdio.h>
 
 #include "fann.h"
 
-int max(float a[]) {
-    int res = 0;
-    for (int i = 1; i < 4; i++)
-        if (a[i] > a[res])
-            res = i;
-    return res;
-}
+inline int check(float predicted[4], float desired[4], int ignore_ct);
+
+inline void process_bar(int x, int n, int r);
 
 int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "usage: %s network data\n", argv[0]);
+        return -1;
+    }
+
+    printf("Loading file...\n");
 
     struct fann *ann = fann_create_from_file(argv[1]);
     struct fann_train_data *test_data = fann_read_train_from_file(argv[2]);
 
-    int ans = 0;
+    printf("Testing...\n");
+
     fann_reset_MSE(ann);
-    for (int i = 0; i < fann_length_train_data(test_data); i++) {
+    int num_correct = 0, ratio = 0, length_data = fann_length_train_data(test_data);
+    for (int i = 0; i < length_data; i++) {
         fann_type *calc_out = fann_run(ann, test_data->input[i]);
         fann_test(ann, test_data->input[i], test_data->output[i]);
-        int bo = 0;
-        if (test_data->output[i][max(calc_out)]) {
-        // try ignore T and C
-        //if (test_data->output[i][max(calc_out)] || (max(calc_out) > 1 && (test_data->output[i][2] || test_data->output[i][3]))) {
-            ans++;
-            bo = 1;
-        }
-        //if (bo) putchar('*'); printf("pred: %f %f %f %f\n", calc_out[0], calc_out[1], calc_out[2], calc_out[3]);
-        //if (bo) putchar('*'); printf("true: %f %f %f %f\n", test_data->output[i][0], test_data->output[i][1], test_data->output[i][2], test_data->output[i][3]);
+        num_correct += check(calc_out, test_data->output[i], 1) ? 1 : 0;
+
+        printf("Accuracy: %d / %d = %5.1f%%\t", num_correct, i, (double)num_correct / i * 100);
+        process_bar(i, length_data / 100, 20);
     }
 
+    printf("Accuracy: %d / %d = %5.1f%%\n", num_correct, length_data, (double)num_correct / length_data * 100);
     printf("MSE error on test data: %f\n", fann_get_MSE(ann));
     printf("bit fail on test data: %u\n", fann_get_bit_fail(ann));
-    printf("%d %lf\n", ans, (double)ans / fann_length_train_data(test_data));
 
     fann_destroy_train(test_data);
     fann_destroy(ann);
 
+    puts("Done!");
+
     return 0;
+}
+
+int check(float predicted[4], float desired[4], int ignore_ct) {
+    int max = 0;
+    for (int i = 1; i < 4; i++)
+        if (predicted[i] > predicted[max])
+            max = i;
+    if (ignore_ct && max >= 2) return desired[2] || desired[3];
+    return desired[max];
+}
+
+void process_bar(int x, int n, int r) {
+    int ratio = x / n;
+    printf("Progress: %3d%%[", ratio);
+    for (int i = 0; i < r; i++) putchar(i < ratio / (100 / r) ? '=' : ' ');
+    printf("]\n\033[F\033[J");
 }
